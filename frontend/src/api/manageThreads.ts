e5f2db2d58035006ee2d0a443ab0e6312bf7a272
JordanChow <jordan.chow@hotmail.com>
2024-02-18 15:02:21 -0500

    update base url

--- frontend/src/api/manageThreads.ts
+++ frontend/src/api/manageThreads.ts
@@ -2,7 +2,7 @@ import { MessageType } from "@/utils/types";
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
